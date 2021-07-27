8df566e9d1f331cbc0b4d57a6fab61300902fc42
Luk0ss <zaidimams.tik@gmail.com>
2021-07-27 14:20:31 +0300

    Added new class Response, added views and layouts, moved index to the public folder

--- core/Router.php
+++ core/Router.php
@@ -5,16 +5,19 @@ namespace app\core;
 class Router
 {
     public Request $request;
+    public Response $response;
 
     protected array $routes = [];
 
     /**
      * Router constructor.
      * @param Request $request
+     * @param Response $response
      */
-    public function __construct(Request $request)
+    public function __construct(Request $request, Response $response)
     {
         $this->request = $request;
+        $this->response = $response;
     }
 
 
@@ -29,10 +32,34 @@ class Router
         $method = $this->request->getMethod();
         $callback = $this->routes[$method][$path] ?? false;
         if($callback === false) {
-            echo "Not Found";
-            exit;
+            $this->response->setStatusCode(404);
+            return 'Not found';
         }
-        echo call_user_func($callback);
+        if(is_string($callback)){
+            return $this->renderView($callback);
+        }
+        return call_user_func($callback);
+    }
+
+    private function renderView($view)
+    {
+        $layoutContent = $this->layoutContent();
+        $viewContent = $this->renderOnlyView($view);
+        return str_replace('{{content}}', $viewContent, $layoutContent);
+    }
+
+    protected function layoutContent()
+    {
+        ob_start();
+        include_once Application::$ROOT_DIR."/views/layouts/main.php";
+        return ob_get_clean();
+    }
+
+    protected function renderOnlyView($view)
+    {
+        ob_start();
+        include_once Application::$ROOT_DIR."/views/$view.php";
+        return ob_get_clean();
     }
 
 }
